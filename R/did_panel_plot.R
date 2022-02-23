18060bac0773a2eafd79cf07389a94df6be23b09
till-tietz <ttietz2014@gmail.com>
2022-02-23 23:29:29 +0100

    added did panel plotting function

---
+++ R/did_panel_plot.R
@@ -0,0 +1,41 @@
+#' generate a DiD panel plot
+#'
+#' @name did_panel_plot
+#'
+#' did_panel_plot generates a plot indicating unit group and treatment status over time
+#'
+#' @param data A df of DiD panel data with unit, group, year and treatment status indicators
+#' @param unit character vector specifying the name of the unit variable
+#' @param year character vector specifying the name of the year variable
+#' @param group character vector specifying the name of the group variable
+#' @param treat character vector specifying the name of the treatment variable
+#' @return a ggplot object
+#' @export
+
+
+did_panel_plot <- function(data, unit, year, group, treat){
+
+  data$x_start <- data[[year]]
+  data$x_end <- data[[year]] + 1
+  data$y_start <- data[[unit]]
+  data$y_end <- data[[unit]]
+  data[[treat]] <- factor(data[[treat]],
+                          labels = c("Under Control","Under Treatment"))
+
+
+  ggplot(data = data, aes(x = x_start, xend = x_end,
+                          y = y_start, yend = y_end,
+                          color = factor(.data[[group]]),
+                          alpha = .data[[treat]]))+
+    geom_segment(key_glyph = "rect")+
+    scale_alpha_discrete("Treatment Status",range = c(0.4,1))+
+    scale_x_continuous(breaks = c(min(data$x_start):max(data$x_end)))+
+    scale_colour_discrete("Group")+
+    ylab("Unit")+
+    xlab("Time")+
+    theme_bw()+
+    guides(color = guide_legend(order = 1),
+           alpha = guide_legend(order = 2))
+
+
+}
