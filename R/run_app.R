b69332f5b6bb3d4db994c8b493e475f10a5949a5
till-tietz <ttietz2014@gmail.com>
2022-02-24 03:16:13 +0100

    finished simulation UI

--- R/run_app.R
+++ R/run_app.R
@@ -9,19 +9,22 @@
 
 
 library(shiny)
+library(shinyWidgets)
 
 run_app <- function(){
 
   ui <- navbarPage("DiDsim",
                    navbarMenu("Menu",
+        # simulation panel -----------------------------------
                               tabPanel("Simulation",
                                        fluidPage(
+              # data input -----------------------------------
                                          sidebarPanel(
                                            width = 4,
-                                           selectInput("g_select","Group:",
+                                           selectInput("g_select","Groups:",
                                                        choices = list(" ","Group 1","Group 2","Group 3"),
                                                        selected = " ",
-                                                       width = "50%"),
+                                                       width = "75%"),
                                            conditionalPanel(
                                              condition = "input.g_select == 'Group 1'",
                                              sliderInput("te1", "Treatment Effect:", 2, min = -10, max = 10),
@@ -36,7 +39,44 @@ run_app <- function(){
                                            ),
                                            conditionalPanel(
                                              condition = "input.g_select == 'Group 3'",
-
+                                             shinyWidgets::materialSwitch(
+                                               inputId = "is_treated3",
+                                               label = "Treated",
+                                               value = FALSE,
+                                               status = "primary"
+                                             ),
+                                             conditionalPanel(
+                                               condition = "input.is_treated3 == true",
+                                               sliderInput("te3", "Treatment Effect:", 2, min = -10, max = 10),
+                                               sliderInput("te_m3", "Treatment Effect Slope:", value = 0.4, min = -1, max = 1, step = 0.05),
+                                               sliderInput("g3", "Treatment Time:", value = 3, min = 1, max = 10, step = 1, sep = "")
+                                             )
+                                           ),
+                                           selectInput("presets","Heterogeneity Presets:",
+                                                       choices = list("None",
+                                                                      "Homogeneous Effects",
+                                                                      "Heterogeneity in Levels",
+                                                                      "Heterogeneity in Levels (w/ Slopes)",
+                                                                      "Heterogeneity in Slopes",
+                                                                      "Heterogeneity in Levels and Slopes"),
+                                                       selected = "None",
+                                                       width = "75%")
+                                         ),
+              # simulation output ---------------------------
+                                         mainPanel(
+                                           width = 8,
+                                           fluidRow(
+                                             plotOutput("sim_did_plot")
+                                           ),
+                                           fluidRow(
+                                             column(4,
+                                                    align = "center",
+                                                    tableOutput("data_params")
+                                             ),
+                                             column(4,
+                                                    align = "center",
+                                                    tableOutput("sim_results")
+                                             )
                                            )
                                          )
                                        )
@@ -52,4 +92,4 @@ run_app <- function(){
 }
 
 
-run_app()
+
